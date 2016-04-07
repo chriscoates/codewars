f15ac776976747e8b19654bf1837e5db7c6660d2
chriselevate <coates.c@hotmail.com>
2016-04-07 17:25:06 +0100

    adding - my head is at the wrong end

---
+++ ruby/8kyu/My head is at the wrong end!.rb
@@ -0,0 +1,34 @@
+#My coode
+def fix_the_meerkat(arr)
+newarray = []
+newarray << arr [2]
+newarray << arr [1]
+newarray << arr [0]
+newarray
+end
+#End my code
+
+=begin task
+My head is at the wrong end!
+Description:
+
+You're at the zoo... all the meerkats look weird. Something has gone terribly wrong - someone has gone and switched their heads and tails around!
+
+Save the animals by switching them back. You will be given an array which will have three values (tail, body, head). It is your job to re-arrange the array so that the animal is the right way round (head, body, tail).
+
+Same goes for all the other arrays/lists that you will get in the tests: you have to change the element positions with the same exact logics - simples!
+=end
+
+=begin other examples
+==============================
+def fix_the_meerkat(arr)
+  arr.reverse
+end
+==============================
+def fix_the_meerkat(arr)
+  return [arr[-1], arr[1], arr[0]]
+end
+==============================
+
+==============================
+=end
