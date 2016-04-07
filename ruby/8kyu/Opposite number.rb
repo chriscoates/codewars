1f9c199fb4d849a65c7052235357b58875855a00
chriselevate <coates.c@hotmail.com>
2016-04-07 17:25:22 +0100

    adding - oppostie number

---
+++ ruby/8kyu/Opposite number.rb
@@ -0,0 +1,36 @@
+#My coode
+def opposite(n)
+  #your code here
+  n * -1
+end
+#End my code
+
+=begin task
+Description:
+
+Very simple, given a number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+But can you do it in 1 line of code and without any conditionals?
+=end
+
+=begin other examples
+==============================
+def opposite n
+  -n
+end
+==============================
+def opposite(x)
+ -1 * x
+end
+==============================
+def opposite(input)
+  return -input
+ #your code here
+end
+==============================
+=end
