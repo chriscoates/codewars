2a32c3c87e6d0fee32693ffcd69d4e043fb3a9bf
chriselevate <coates.c@hotmail.com>
2016-04-07 17:23:54 +0100

    adding - is this my tail

---
+++ ruby/8kyu/Is this my tail?.rb
@@ -0,0 +1,29 @@
+#My coode
+def correct_tail(body, tail)
+  body.upcase.split('').last == tail.upcase
+end
+#End my code
+
+=begin task
+Description:
+
+Some new animals have arrived at the zoo. The zoo keeper is concerned that perhaps the animals do not have the right tails. To help her, you must correct the broken function to make sure that the second argument (tail), is the same as the last letter of the first argument (body) - otherwise the tail wouldn't fit!
+
+If the tail is right return true, else return false.
+
+The arguments will always be strings, and normal letters.
+=end
+
+=begin other examples
+==============================
+def correct_tail(body, tail)
+  body[-1] == tail
+end
+==============================
+def correct_tail(body, tail)
+  body.end_with? tail
+end
+==============================
+
+==============================
+=end
