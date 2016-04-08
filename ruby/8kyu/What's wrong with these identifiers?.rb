d041e9a997686cd51487c671a43ca06a3eca4c49
chriselevate <coates.c@hotmail.com>
2016-04-08 14:57:18 +0100

    whats wrong with these identifiers

---
+++ ruby/8kyu/What's wrong with these identifiers?.rb
@@ -0,0 +1,28 @@
+#My coode
+Person = {
+"1stname" => "John",
+"second-name" => "Doe",
+"email@ddress" => "john.doe@email.com",
+"male.female" => "M"
+}
+#End my code
+
+=begin Description
+An identifier is simply a name...
+Can you amend this object so that it's properties comprise of vaild identifiers?
+=end
+
+=begin other examples
+==============================
+Person = {
+  :firstname => nil,
+  :second_name => nil,
+  :emailaddress => nil,
+  :male_female => nil
+}
+==============================
+Person = { a:nil,b:nil,c:nil,d:nil }
+==============================
+
+==============================
+=end
