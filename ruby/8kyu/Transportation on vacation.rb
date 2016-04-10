58bfad8186e4ba3da335f550ce701851e9555927
chriselevate <coates.c@hotmail.com>
2016-04-10 19:22:20 +0100

    adding - transportation on vocation

---
+++ ruby/8kyu/Transportation on vacation.rb
@@ -0,0 +1,40 @@
+#My coode
+def rental_car_cost(d)
+  if d <= 2
+    d * 40
+  elsif d <=6
+    (d * 40) - 20
+  else
+    (d * 40) - 50
+  end
+end
+#End my code
+
+=begin Description
+After a hard quarter in the office you decide to get some rest on a vacation. So you will book a flight for you and your girlfriend and try to leave all the mess behind you.
+
+You will need a rental car in order for you to get around in your vacation. The manager of the car rental makes you some good offers.
+
+Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total. Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
+
+Write a code that gives out the total amount for different days(d).
+=end
+
+=begin other examples
+==============================
+def rental_car_cost(d)
+  return d * 40 if d < 3
+  return d * 40 - 20 if d < 7
+  return d * 40 - 50
+end
+==============================
+def rental_car_cost(d)
+  total = d*40
+  total -= 20 if d >= 3
+  total -= 30 if d >= 7
+  total
+end
+==============================
+
+==============================
+=end
